9e07c1b7cbb541cf40ed8ad8ec28006cfcd546e5
Iván López <lopezi@objectcomputing.com>
2019-11-20 10:56:38 +0100

    Fix asciidoctor callout

--- complete/bookcatalogue/src/test/groovy/example/micronaut/bookcatalogue/BooksControllerSpec.groovy
+++ complete/bookcatalogue/src/test/groovy/example/micronaut/bookcatalogue/BooksControllerSpec.groovy
@@ -19,7 +19,7 @@ class BooksControllerSpec extends Specification {
     void "it is possible to retrieve books"() {
         when:
         HttpRequest request = HttpRequest.GET("/books") // <3>
-        List books = client.toBlocking().retrieve(request, Argument.listOf(Book)) // <3>
+        List books = client.toBlocking().retrieve(request, Argument.listOf(Book)) // <4>
 
         then:
         books.size() == 3
